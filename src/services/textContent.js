bb841d098af873a82315e0018f95a36c5d97501b
Ivan <malov-van@mail.ru>
2022-06-13 03:25:32 +0300

    Добавлены новые сектора

--- src/services/textContent.js
+++ src/services/textContent.js
@@ -1,197 +1,237 @@
 const textContent = {
-    'L0-CIO': {
-       id:'L0-CIO',
-       name:'Директор по ИТ',
-    },
-    'L1-CDO':{
-       id:'L1-CDO',
-       name:'todo name'
-    },
-    'L1-EMPTY':{
-       id:'L1-EMPTY',
-       name:'todo name'
-    },
-    'L1-HD':{
-       id:'L1-HD',
-       name:'todo name'
-    },
-    'L1-HISO':{
-       id:'L1-HISO',
-       name:'todo name'
-    },
-    'L1-HITPS':{
-       id:'L1-HITPS',
-       name:'todo name'
-    },
-    'L1-HPMO':{
-       id:'L1-HPMO',
-       name:'todo name'
-    },
-    'L1_HT':{
-       id:'L1_HT',
-       name:'todo name'
-    },
-    'L2-A':{
-       id:'L2-A',
-       name:'todo name'
-    },
-    'L2-AGILE':{
-       id:'L2-AGILE',
-       name:'todo name'
-    },
-    'L2-ITSA':{
-       id:'L2-ITSA',
-       name:'todo name'
-    },
-    'L2-PPM':{
-       id:'L2-PPM',
-       name:'todo name'
-    },
-    'L2-TL1':{
-       id:'L2-TL1',
-       name:'todo name'
-    },
-    'L2-TL2':{
-       id:'L2-TL2',
-       name:'todo name'
-    },
-    'L2-TL3':{
-       id:'L2-TL3',
-       name:'todo name'
-    },
-    'L2-TL4':{
-       id:'L2-TL4',
-       name:'todo name'
-    },
-    'L2-TL5':{
-       id:'L2-TL5',
-       name:'todo name'
-    },
-    'L2-TL6':{
-       id:'L2-TL6',
-       name:'todo name'
-    },
-    'L3-DA':{
-       id:'L3-DA',
-       name:'todo name'
-    },
-    'L3-DME':{
-       id:'L3-DME',
-       name:'todo name'
-    },
-    'L3-DO':{
-       id:'L3-DO',
-       name:'todo name'
-    },
-    'L3-DS':{
-       id:'L3-DS',
-       name:'todo name'
-    },
-    'L3-H':{
-       id:'L3-H',
-       name:'todo name'
-    },
-    'L3-ISC':{
-       id:'L3-ISC',
-       name:'todo name'
-    },
-    'L3-ITSM':{
-       id:'L3-ITSM',
-       name:'todo name'
-    },
-    'L3-L1':{
-       id:'L3-L1',
-       name:'todo name'
-    },
-    'L3-L2':{
-       id:'L3-L2',
-       name:'todo name'
-    },
-    'L3-L3':{
-       id:'L3-L3',
-       name:'todo name'
-    },
-    'L3-PM':{
-       id:'L3-PM',
-       name:'todo name'
-    },
-    'L3-PT':{
-       id:'L3-PT',
-       name:'todo name'
-    },
-    'L3-S':{
-       id:'L3-S',
-       name:'todo name'
-    },
-    'L3-SD':{
-       id:'L3-SD',
-       name:'todo name'
-    },
-    'L3-SOC':{
-       id:'L3-SOC',
-       name:'todo name'
-    },
-    'L3-SQAE':{
-       id:'L3-SQAE',
-       name:'todo name'
-    },
-    'L3-T':{
-       id:'L3-T',
-       name:'todo name'
-    },
-    'L4-MD':{
-       id:'L4-MD',
-       name:'todo name'
-    },
-    'L4-MQAE':{
-       id:'L4-MQAE',
-       name:'todo name'
-    },
-    'L5-BA':{
-       id:'L5-BA',
-       name:'todo name'
-    },
-    'L5-JD':{
-       id:'L5-JD',
-       name:'todo name'
-    },
-    'L5-JQAE':{
-       id:'L5-JQAE',
-       name:'todo name'
-    },
-    'L5-SA':{
-       id:'L5-SA',
-       name:'todo name'
-    },
-    'L6-AS':{
-       id:'L6-AS',
-       name:'todo name'
-    },
-    'L6-ITIS':{
-       id:'L6-ITIS',
-       name:'todo name'
-    },
-    'L6-SASEECT': {
-       id:'L6-SASEECT',
-       name:'todo name'
-    },
-    'L6-T1':{
-       id:'L6-T1',
-       name:'todo name'
-    },
-    'L6-T2':{
-       id:'L6-T2',
-       name:'todo name'
-    },
-    'L6-TW':{
-       id:'L6-TW',
-       name:'todo name'
-    },
-    'L6-US':{
-       id:'L6-US',
-       name:'todo name'
-    }
- }
+   'L0-CIO': {
+      id: 'L0-CIO',
+      name: 'Директор по ИТ'
+   },
+   'L1-CDO': {
+      id: 'L1-CDO',
+      name: 'todo name'
+   },
+   'L1-EMPTY': {
+      id: 'L1-EMPTY',
+      name: 'todo name'
+   },
+   'L1-HD': {
+      id: 'L1-HD',
+      name: 'todo name'
+   },
+   'L1-HISO': {
+      id: 'L1-HISO',
+      name: 'todo name'
+   },
+   'L1-HITPS': {
+      id: 'L1-HITPS',
+      name: 'todo name'
+   },
+   'L1-HPMO': {
+      id: 'L1-HPMO',
+      name: 'todo name'
+   },
+   'L1_HT': {
+      id: 'L1_HT',
+      name: 'todo name'
+   },
+   'L2-A': {
+      id: 'L2-A',
+      name: 'todo name'
+   },
+   'L2-AGILE': {
+      id: 'L2-AGILE',
+      name: 'todo name'
+   },
+   'L2-ITSA': {
+      id: 'L2-ITSA',
+      name: 'todo name'
+   },
+   'L2-PPM': {
+      id: 'L2-PPM',
+      name: 'todo name'
+   },
+   'L2-TL1': {
+      id: 'L2-TL1',
+      name: 'todo name'
+   },
+   'L2-TL2': {
+      id: 'L2-TL2',
+      name: 'todo name'
+   },
+   'L2-TL3': {
+      id: 'L2-TL3',
+      name: 'todo name'
+   },
+   'L2-TL4': {
+      id: 'L2-TL4',
+      name: 'todo name'
+   },
+   'L2-TL5': {
+      id: 'L2-TL5',
+      name: 'todo name'
+   },
+   'L2-TL6': {
+      id: 'L2-TL6',
+      name: 'todo name'
+   },
+   'L3-DA': {
+      id: 'L3-DA',
+      name: 'todo name'
+   },
+   'L3-DME': {
+      id: 'L3-DME',
+      name: 'todo name'
+   },
+   'L3-DO': {
+      id: 'L3-DO',
+      name: 'todo name'
+   },
+   'L3-DS': {
+      id: 'L3-DS',
+      name: 'todo name'
+   },
+   'L3-H': {
+      id: 'L3-H',
+      name: 'todo name'
+   },
+   'L3-ISC': {
+      id: 'L3-ISC',
+      name: 'todo name'
+   },
+   'L3-ITSM': {
+      id: 'L3-ITSM',
+      name: 'todo name'
+   },
+   'L3-L1': {
+      id: 'L3-L1',
+      name: 'todo name'
+   },
+   'L3-L2': {
+      id: 'L3-L2',
+      name: 'todo name'
+   },
+   'L3-L3': {
+      id: 'L3-L3',
+      name: 'todo name'
+   },
+   'L3-PM': {
+      id: 'L3-PM',
+      name: 'todo name'
+   },
+   'L3-PT': {
+      id: 'L3-PT',
+      name: 'todo name'
+   },
+   'L3-S': {
+      id: 'L3-S',
+      name: 'todo name'
+   },
+   'L3-SD': {
+      id: 'L3-SD',
+      name: 'todo name'
+   },
+   'L3-SOC': {
+      id: 'L3-SOC',
+      name: 'todo name'
+   },
+   'L3-SQAE': {
+      id: 'L3-SQAE',
+      name: 'todo name'
+   },
+   'L3-T': {
+      id: 'L3-T',
+      name: 'todo name'
+   },
+   'L4-MD': {
+      id: 'L4-MD',
+      name: 'todo name'
+   },
+   'L4-MQAE': {
+      id: 'L4-MQAE',
+      name: 'todo name'
+   },
+   'L5-BA': {
+      id: 'L5-BA',
+      name: 'todo name'
+   },
+   'L5-JD': {
+      id: 'L5-JD',
+      name: 'todo name'
+   },
+   'L5-JQAE': {
+      id: 'L5-JQAE',
+      name: 'todo name'
+   },
+   'L5-SA': {
+      id: 'L5-SA',
+      name: 'todo name'
+   },
+   'L6-AS': {
+      id: 'L6-AS',
+      name: 'todo name'
+   },
+   'L6-ITIS': {
+      id: 'L6-ITIS',
+      name: 'todo name'
+   },
+   'L6-SASEECT': {
+      id: 'L6-SASEECT',
+      name: 'todo name'
+   },
+   'L6-T1': {
+      id: 'L6-T1',
+      name: 'todo name'
+   },
+   'L6-T2': {
+      id: 'L6-T2',
+      name: 'todo name'
+   },
+   'L6-TW': {
+      id: 'L6-TW',
+      name: 'todo name'
+   },
+   'L6-US': {
+      id: 'L6-US',
+      name: 'todo name'
+   },
+   'L7-D': {
+      id: 'L7-D',
+      name: 'todo name'
+   },
+   'L7-DA': {
+      id: 'L7-DA',
+      name: 'todo name'
+   },
+   'L7-IS': {
+      id: 'L7-IS',
+      name: 'todo name'
+   },
+   'L7-ITOAS': {
+      id: 'L7-ITOAS',
+      name: 'todo name'
+   },
+   'L7-PM': {
+      id: 'L7-PM',
+      name: 'todo name'
+   },
+   'L7-QA': {
+      id: 'L7-QA',
+      name: 'todo name'
+   },
+   'L7-T': {
+      id: 'L7-T',
+      name: 'todo name'
+   },
+   'MagicCircle': {
+      id: 'MagicCircle',
+      name: 'todo name'
+   },
+   'app': {
+      id: 'app',
+      name: 'todo name'
+   },
+   'inset-shadow': {
+      id: 'inset-shadow',
+      name: 'todo name'
+   }
+}
 export default {
     textContent
 }
